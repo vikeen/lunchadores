461ca6f89a40f89599dc751864e82a61a17639e2
John Rake <john.rake12@gmail.com>
2015-12-20 19:09:50 -0600

    Adding travis.ci configuration and steps to create working travis.ci integration

---
+++ server/config/environment/travis.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+    database: {
+        uri: 'postgres://postgres@localhost:5432/travis_ci_test'
+    }
+};
