281a887bb908d6c863c9003d8965878fb8ca9cde
John Rake <john.rake12@gmail.com>
2015-12-21 12:55:12 -0600

    Ignore image versions for now to not screw up default restaurant images

--- grunt/config/rev.js
+++ grunt/config/rev.js
@@ -6,8 +6,7 @@ module.exports = {
             src: [
                 '<%= yeoman.dist %>/public/{,*/}*.js',
                 '<%= yeoman.dist %>/public/{,*/}*.css',
-                '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-                '<%= yeoman.dist %>/public/assets/fonts/*'
+                //'<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             ]
         }
     }
